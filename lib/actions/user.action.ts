1bbf8c8ad7be9515762eef3a78cebf15bb9d2a55
KiranPK12 <kiranpadma2003@gmail.com>
2024-01-19 10:01:06 +0530

    initial commit

---
+++ lib/actions/user.action.ts
@@ -0,0 +1,62 @@
+"use server";
+
+import { CreateUserParams, UpdateUserParams } from "@/types";
+import { handleError } from "../utils";
+import { connectToDatabase } from "../database";
+import User from "../database/models/user.model";
+import { revalidatePath } from "next/cache";
+import Event from "../database/models/event.model";
+import Order from "../database/models/order.model";
+import prisma from "../db";
+
+export const createUser = async (user: any) => {
+  try {
+    const newUser = await prisma.user.create(user);
+    if (!newUser) throw new Error("No User Found");
+    
+    return JSON.parse(JSON.stringify(newUser));
+  } catch (error) {
+    handleError(error);
+  }
+};
+// update user
+export const updateUser = async (clerkId: string, user: UpdateUserParams) => {
+  try {
+    await connectToDatabase();
+    const updatedUser = await User.findOneAndUpdate({ clerkId }, user, {
+      new: true,
+    });
+
+    if (!updateUser) throw new Error("User update failed");
+    return JSON.parse(JSON.stringify(updatedUser));
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export const deleteUser = async (clerkId: string) => {
+  try {
+    await connectToDatabase();
+    const userToDelete = await User.findOne({ clerkId });
+
+    if (!userToDelete) throw new Error("User not Found");
+    await Promise.all([
+      Event.updateMany(
+        { _id: { $in: userToDelete.events } },
+        { $pull: { organizer: userToDelete._id } }
+      ),
+
+      Order.updateMany(
+        { _id: { $in: userToDelete.orders } },
+        { $unset: { buyer: 1 } }
+      ),
+    ]);
+
+    const deletedUser = await User.findByIdAndDelete(userToDelete._id);
+    revalidatePath("/");
+
+    return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null;
+  } catch (error) {
+    handleError(error);
+  }
+};
